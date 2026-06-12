b363940a324762fdb79a874a1bcb9f353bb68b05
ncasanovas <nataliasolcasanovas@gmail.com>
2026-06-12 09:42:37 -0300

    feat: Add App Config

---
+++ src/main/java/com/ncasanovas/jwt_token/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.ncasanovas.jwt_token.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+  
+}
