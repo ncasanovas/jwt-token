99827ad7272b604ce1de314e8b3fa0070730657a
ncasanovas <nataliasolcasanovas@gmail.com>
2026-07-17 19:33:24 -0300

    fix: Fix name methods fom token repository

--- src/main/java/com/ncasanovas/jwt_token/repository/TokenRepository.java
+++ src/main/java/com/ncasanovas/jwt_token/repository/TokenRepository.java
@@ -1,6 +1,7 @@
 package com.ncasanovas.jwt_token.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,8 @@ import com.ncasanovas.jwt_token.model.Token;
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
-  List<Token> findAllValidTokenByUser(Long id);
+  List<Token> findAllValidIsFalseOrRevokedIsFalseByUserId(Long id);
+
+  Optional<Token> findByToken(String token);
 
 }
